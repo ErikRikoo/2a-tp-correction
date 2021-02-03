5ad35e6211a8fb09e5ad505ae0ea7ae8cb4dc771
erik <erik@pristimantis.com>
2021-02-03 16:56:49 +0100

    Le bouton fonctionne et peut faire apparaitre le bon objet

--- Tp-2A-Correction/Assets/Scripts/Environnement/Trigger.cs
+++ Tp-2A-Correction/Assets/Scripts/Environnement/Trigger.cs
@@ -1,9 +1,15 @@
-﻿using UnityEngine;
+﻿using System;
+using UnityEngine;
 
 namespace Environnement
 {
     public class Trigger : MonoBehaviour
     {
-        
+        [SerializeField] private GameObject m_ObjectToTrigger;
+
+        private void OnTriggerEnter2D(Collider2D other)
+        {
+            m_ObjectToTrigger.SetActive(true);
+        }
     }
 }
