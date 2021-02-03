dbe2c80edf12684c6675300db92b43d9a535506b
erik <erik@pristimantis.com>
2021-02-03 18:28:31 +0100

    On a rajouté la possibilité de bouger un objet vers le haut: On voit rapidement qu'il y a un soucis d'architecture

--- Tp-2A-Correction/Assets/Scripts/Environnement/Trigger.cs
+++ Tp-2A-Correction/Assets/Scripts/Environnement/Trigger.cs
@@ -7,6 +7,7 @@ namespace Environnement
     {
         APPEAR,
         DISAPPEAR,
+        MOVE_TOP, // On voit bien qu'à chaque nouvelle fonctionnalité on doit changer l'enum
     }
     
     public class Trigger : MonoBehaviour
@@ -14,10 +15,18 @@ namespace Environnement
         [SerializeField] private GameObject m_ObjectToTrigger;
         [SerializeField] private TriggerAction m_TriggerAction;
         
+        // On rajoute cette valeur qui est alors inutile si on ne veut pas bouger l'objet 
+        [SerializeField] private float m_TopMovementOffset = 2f;
+        
 
         private void OnTriggerEnter2D(Collider2D other)
         {
-            // Maintenant on fait apparaitre ou disparaitre l'obstacle en fonction de l'enum
+            // Mais on doit aussi changer l'implémentation
+            // On voit bien ici qu'il y a un problème
+            // Ce n'est absolument pas modulable
+            // Ce n'est pas designer friendly
+            // Ca viole le principe d'Open-Closed de SOLID
+            // Et on pourrait appliquer le principe d'inversion de dépendance (SOLID)
             switch (m_TriggerAction)
             {
                 case TriggerAction.APPEAR:
@@ -25,6 +34,9 @@ namespace Environnement
                     break;
                 case TriggerAction.DISAPPEAR:
                     m_ObjectToTrigger.SetActive(false);
+                    break;
+                case TriggerAction.MOVE_TOP:
+                    m_ObjectToTrigger.transform.position += Vector3.up * m_TopMovementOffset;
                     break;
             }
         }
