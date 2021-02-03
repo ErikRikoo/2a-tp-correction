93450bb4e0e69c1c5285a53f4a6808ed9d21276a
erik <erik@pristimantis.com>
2021-02-03 17:10:31 +0100

    Maintenant on peut à la fois faire apparaitre ou disparaitre

--- Tp-2A-Correction/Assets/Scripts/Environnement/Trigger.cs
+++ Tp-2A-Correction/Assets/Scripts/Environnement/Trigger.cs
@@ -1,16 +1,32 @@
-using System;
+﻿using System;
 using UnityEngine;
 
 namespace Environnement
 {
+    enum TriggerAction
+    {
+        APPEAR,
+        DISAPPEAR,
+    }
+    
     public class Trigger : MonoBehaviour
     {
         [SerializeField] private GameObject m_ObjectToTrigger;
+        [SerializeField] private TriggerAction m_TriggerAction;
+        
 
         private void OnTriggerEnter2D(Collider2D other)
         {
-            // Pour le moment on fait juste apparaitre un bouton
-            m_ObjectToTrigger.SetActive(true);
+            // Maintenant on fait apparaitre ou disparaitre l'obstacle en fonction de l'enum
+            switch (m_TriggerAction)
+            {
+                case TriggerAction.APPEAR:
+                    m_ObjectToTrigger.SetActive(true);
+                    break;
+                case TriggerAction.DISAPPEAR:
+                    m_ObjectToTrigger.SetActive(false);
+                    break;
+            }
         }
     }
 }
